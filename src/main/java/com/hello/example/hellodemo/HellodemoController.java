e7fcd07be1aefd172b9ce0f1f9be5f8f3f9c8f98
anupam-170490 <74314156+anupam-170490@users.noreply.github.com>
2020-11-17 19:58:45 +0530

    second commit

--- src/main/java/com/hello/example/hellodemo/HellodemoController.java
+++ src/main/java/com/hello/example/hellodemo/HellodemoController.java
@@ -8,7 +8,7 @@ public class HellodemoController {
 	
 	@GetMapping("/hello")
     public String hello() {
-        return "Hello World RESTful with Spring Boot";
+        return "Welcome To Universe Anupam";
     }
 
 }
